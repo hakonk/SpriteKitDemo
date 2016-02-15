365d4721c993a9ec7dfa36fd56801ed4f1d17cfa
hakonk <hakon.knutzen@gmail.com>
2016-02-15 19:16:21 +0100

    Added tvos target and main scene

--- SpriteKitDemo/ViewController.swift
+++ SpriteKitDemo/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("loaded \(self)")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
