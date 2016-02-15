cae29d5b48d0228e26cb43428aa62bc1e2f42dba
Håkon Knutzen <hakn@netlight.com>
2016-02-15 18:46:12 +0100

    Initial Commit

---
+++ SpriteKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpriteKitDemo
+//
+//  Created by Håkon Knutzen on 15/02/16.
+//  Copyright © 2016 Netlight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
