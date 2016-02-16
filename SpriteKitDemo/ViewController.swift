c3b16cd2c79730c7786e87adcd6c0ecd51a75ee2
hakonk <hakon.knutzen@gmail.com>
2016-02-16 23:30:40 +0100

    Made screensaver

--- SpriteKitDemo/ViewController.swift
+++ SpriteKitDemo/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("loaded \(self)")
+        if let scene = SKScene(fileNamed:"MainScene") {
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            skView.ignoresSiblingOrder = true
+            let action = SKAction(named: "rotateAndScale")
+            scene.scaleMode = .AspectFill
+            let netlightLogo = scene.childNodeWithName("SKSpriteNode_0")
+            skView.presentScene(scene)
+            netlightLogo?.runAction(SKAction.repeatActionForever(action!))
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
